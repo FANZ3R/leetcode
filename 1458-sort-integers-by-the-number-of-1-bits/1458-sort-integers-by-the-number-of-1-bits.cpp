c4426b400a74c9d284fe0aaa6a48e540ca13a941
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-11 19:17:54 +0530

    1458-SORT-INTEGERS-BY-THE-NUMBER-OF-1-BITS Stats: Time: 0 ms (100%), Space: 12.8 MB (73.75%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-
-    int find1bits(int x)
-    {
-        int count=0;
-
-        while(x)
-        {
-            if((x&1)==1)
-            count++;
-
-            x=x>>1;
-        }
-        return count;
-    }
-
     vector<int> sortByBits(vector<int>& arr) {
 
-        //comparator use karna padega to sort on the basis of count of one bits
+        //lambda fucntion banunaga sort karne ke lie on the basis of count of 1 bits
 
-
-
-        auto lambda=[&](int &a,int &b)
+        auto lambda = [&] (int &a,int &b)
         {
-            int ca = find1bits(a);
-            int cb = find1bits(b);
+            int ca = __builtin_popcount(a);
+            int cb = __builtin_popcount(b);
 
-            if(ca == cb)
-                return a<b;
-            
+            if(ca==cb)
+            return a<b;
+
             return ca<cb;
         };
 
         sort(arr.begin(),arr.end(),lambda);
 
         return arr;
-
-        
-
-
         
     }
 };
