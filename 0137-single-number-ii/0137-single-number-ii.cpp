69057d5e4a6bc77220c1608fab37236e9ead9b53
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-11 10:16:05 +0530

    Time: 11 ms (12.55%), Space: 12.1 MB (57.08%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,22 +1,47 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
-        unordered_map<int,int> mp;
-
-        for(int x:nums)
-        {
-            mp[x]++;
-        }
-
-        //int ans=0;
-        for(auto t:mp)
-        {
-            if(t.second==1)
-            return t.first;
-        }
-
-        return 0;
         
+
+        //standard approach this works best for both single number 1 and single number 2
+
+        //32 bit ka number derakha hai most
+        //so ham har bit [e kaayenge 32] aur bit pe jaake 
+        //har numebr ke we will count no of 1 bit and 0 bit
+        //agr countof1's %3==1 hota hai toh mtlb hamare target number pe is bit pe 1 hai
+
+        //toh ham har bit ka nikaal nikaal kr OR karte rahnege at the end
+
+        int result=0;
+
+        for(int i=0;i<=31;i++)
+        {
+            int temp=1<<i;//1 ko left shift karunga i se har ith bit nikaalne ko
+
+            int countZeros=0;
+            int countOnes=0;
+
+            for(int &num : nums)
+            {
+                if((num & temp)==0)
+                {
+                    countZeros++;
+                }
+                else
+                {
+                    countOnes++;
+                }
+            }
+
+            //ab saare count pata chlgya agr mod lene pe 3 se 1 ata to mtlb target number ke ith bit pe 1 hai
+            if(countOnes%3==1)
+            {
+                result=result | temp;
+                //toh ham wo waali bit OR kardenge apne result mei
+                //aise karte jayenge jab pata chalega resulting number pe yha pe 1 hai
+            }
+        }
+
+        return result;
     }
 };
