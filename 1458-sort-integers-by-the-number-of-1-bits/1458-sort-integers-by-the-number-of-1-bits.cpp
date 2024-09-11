21625dce7c8185c4a4b3f4e17bf704d2dbd620c2
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-11 19:22:17 +0530

    1458-SORT-INTEGERS-BY-THE-NUMBER-OF-1-BITS Stats: Time: 6 ms (68.01%), Space: 12.9 MB (73.75%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
+
+    // int find1bits(int x)
+    // {
+    //     int count=0;
+
+    //     while(x)
+    //     {
+    //         if((x&1)==1)
+    //         count++;
+
+    //         x=x>>1;
+    //     }
+    //     return count;
+    // }
+
+    //mai bina function ke karunga
+    //aur is utility se karna must hai kyuki ye dedeta hai set bits of a number in O(1)
+
+    //is utility ko use karte as __builtin_popcount(int x) -->ye x ke set bits ka count dedega.
+
+
     vector<int> sortByBits(vector<int>& arr) {
 
-        //lambda fucntion banunaga sort karne ke lie on the basis of count of 1 bits
+        //comparator use karna padega to sort on the basis of count of one bits
 
-        auto lambda = [&] (int &a,int &b)
+
+
+        auto lambda=[&](int &a,int &b)
         {
             int ca = __builtin_popcount(a);
-            int cb = __builtin_popcount(b);
+            int cb =  __builtin_popcount(b);
 
-            if(ca==cb)
-            return a<b;
-
+            if(ca == cb)
+                return a<b;
+            
             return ca<cb;
         };
 
         sort(arr.begin(),arr.end(),lambda);
 
         return arr;
+
+        
+
+
         
     }
 };
