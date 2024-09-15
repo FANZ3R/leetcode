492b658070b705eeeffa1bdeed24edf25eb0cf76
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-15 14:54:28 +0530

    1473-FIND-THE-LONGEST-SUBSTRING-CONTAINING-VOWELS-IN-EVEN-COUNTS Stats: Time: 83 ms (37.67%), Space: 18.9 MB (32.79%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -2,81 +2,59 @@ class Solution {
 public:
     int findTheLongestSubstring(string s) {
 
-        //isko sliding window se nahi banaskta kyuki manle abhi vowel odd aare par baad mei even aaskte
-        //but sliding window waale methode se mai skip kardunga abhi9 odd pe hi
+        //direct integer mask leke bhi hojaaega ye
+        
+        //mai mask ki msb ko a se represent karunga
+        //00000
+        //uoiea //aise represent honge ye
 
-        //ab mai isko map leke bana skta string ka 
-        //string mei mai sirf bits store karunga vowels ki aur unki bit maintain karta rahugna
-        //00000  represent karega aeiou ko 0 ke lie even 1 ke lie odd
+        //aur map mei index store karunga ki phle ye state dekha hai kabhi 
 
-        unordered_map<string,int>mp;
+        unordered_map<int,int> mp;
+        int mask=0;
 
-        string currstate="00000";
-
-        mp[currstate]=-1;
-
-        //by default saare 0 honge to saare even hi hai
-        //toh usko pehle hi daaldenge compare krne ke lie if we get string 1 character ya further jo maintaing 
-        //krti ho ye rule
-
-        //ab state ke lie bits ko banayenge integer vector
-
-        vector<int> state(5,0);
-
-        //00000 aeiou in order maintain karega
+        mp[mask]=-1;
 
         int ans=0;
+
 
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='a')
             {
-                state[0]=state[0]^1;
-                // %2 se hamesa even odd ka maintain rakhunga mai
+                mask=mask^(1<<0);
+                //a kyuki msb hai toh usko 0 time shift krke
             }
-             if(s[i]=='e')
+            else if(s[i]=='e')
             {
-                state[1]=state[1]^1;
-                
+                mask=mask^(1<<1);
             }
-             if(s[i]=='i')
+            else if(s[i]=='i')
             {
-                state[2]=state[2]^1;
-                
+                mask=mask^(1<<2);
             }
-             if(s[i]=='o')
+            else if(s[i]=='o')
             {
-                state[3]=state[3]^1;
-                
+                mask= mask^(1<<3);
+
             }
-             if(s[i]=='u')
+            else if(s[i]=='u')
             {
-                state[4]=state[4]^1;
-                
+                mask=mask^(1<<4);
             }
 
-
-            string currstate="";
-
-            //ab currstate build karunga
-
-            for(int j=0;j<5;j++)
+            if(mp.find(mask)!=mp.end())
             {
-                currstate+=to_string(state[j]);
-            }
-
-            if(mp.find(currstate)!=mp.end())
-            {
-                ans=max(ans,(i-mp[currstate]));
+                ans=max(ans,(i-mp[mask]));
             }
             else
             {
-                mp[currstate]=i;
+                mp[mask]=i;
             }
+
         }
 
         return ans;
 
-        
     }
 };
