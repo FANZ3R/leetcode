f545f58f649ddc94095e944f9cbc4ce99adbda88
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-15 20:10:02 +0530

    Time: 0 ms (100%), Space: 9.6 MB (44.29%) - LeetHub

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -2,22 +2,41 @@ class Solution {
 public:
     int countTriplets(vector<int>& arr) {
 
-        int n=arr.size();
+        vector<int> prefixXor(arr.begin(),arr.end());
+
+        prefixXor.insert(prefixXor.begin(),0);
+
+        int n=prefixXor.size();
+
+        //ab m,ai prefix xor calculate kardunga
+        //maine 0 isilie daala kyuki agr wapis dekhunga toh compare karsakunga
+        //kyuki starting mei to 0 hi rahega
+
+        for(int i=1;i<n;i++)
+        {
+            prefixXor[i]^=prefixXor[i-1];
+        }
+
+        //ab traverse karunga aur check karunga firse dekha hai ki nahi
 
         int ans=0;
 
         for(int i=0;i<n;i++)
         {
-            int t=arr[i];
-
+            int val=prefixXor[i];
             for(int k=i+1;k<n;k++)
             {
-                t^=arr[k];
-                if(t==0)
-                    ans+=k-i;//dekh beech mei mai j daalskta hu jitne valid honge toh triplet banane ke lie aayenge merpe k-i 
+                if(prefixXor[k]==val)
+                {
+                    ans+=k-i-1;
+                    //yaha pe -1 kyu kyuki maine ek extra 0 append kiya hai wo window ko hamesa badadega 1 se
+                    //aur tri[plet bhi ek se badh jayenge uske reason se
+                }
             }
         }
-        
+
         return ans;
+
+        
     }
 };
