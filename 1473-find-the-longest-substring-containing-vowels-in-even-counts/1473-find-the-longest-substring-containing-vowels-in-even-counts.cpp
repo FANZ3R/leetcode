7f333560e2c6e4fd1969fa226c6bc97abef77ee6
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-15 14:40:24 +0530

    1473-FIND-THE-LONGEST-SUBSTRING-CONTAINING-VOWELS-IN-EVEN-COUNTS Stats: Time: 768 ms (5.12%), Space: 21.3 MB (6.74%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,42 +1,82 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        //isme ham bitmask banakae kaam karenge aur fir agr hamne wo bitmask phle hi dekha hua hoga toh
-        //usko ham
-        
 
-        map<int,int> mp;
-        //ye store karega mera maine ye mask phle dekha hai kis index pe
+        //isko sliding window se nahi banaskta kyuki manle abhi vowel odd aare par baad mei even aaskte
+        //but sliding window waale methode se mai skip kardunga abhi9 odd pe hi
 
-        //aur mera mask 0 wala even wala hoga har vowel pe
-        //toh by default even wale ko daaldete 
-        //ki agr phle dekha ho kabhi isko 
-        //toh add up hota hi rahega
+        //ab mai isko map leke bana skta string ka 
+        //string mei mai sirf bits store karunga vowels ki aur unki bit maintain karta rahugna
+        //00000  represent karega aeiou ko 0 ke lie even 1 ke lie odd
 
-        mp[0]=-1;
+        unordered_map<string,int>mp;
 
-        int count=0;
+        string currstate="00000";
+
+        mp[currstate]=-1;
+
+        //by default saare 0 honge to saare even hi hai
+        //toh usko pehle hi daaldenge compare krne ke lie if we get string 1 character ya further jo maintaing 
+        //krti ho ye rule
+
+        //ab state ke lie bits ko banayenge integer vector
+
+        vector<int> state(5,0);
+
+        //00000 aeiou in order maintain karega
+
         int ans=0;
 
         for(int i=0;i<s.size();i++)
         {
-            //ab maai bit mask tabhi change karunga agr vowel hai kyuki maine poora 26 bits ka mask leliya
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
-                count=count^(1<<((s[i]-'a')));
-            if(mp.count(count))//agr uska count 0 se jaada hai toh dekha hua hai usko mtlb
+            if(s[i]=='a')
             {
-                ans=max(ans,(i-mp[count]));
+                state[0]=(state[0]+1)%2;
+                // %2 se hamesa even odd ka maintain rakhunga mai
+            }
+             if(s[i]=='e')
+            {
+                state[1]=(state[1]+1)%2;
                 
+            }
+             if(s[i]=='i')
+            {
+                state[2]=(state[2]+1)%2;
+                
+            }
+             if(s[i]=='o')
+            {
+                state[3]=(state[3]+1)%2;
+                
+            }
+             if(s[i]=='u')
+            {
+                state[4]=(state[4]+1)%2;
+                
+            }
+
+
+            string currstate="";
+
+            //ab currstate build karunga
+
+            for(int j=0;j<5;j++)
+            {
+                currstate+=to_string(state[j]);
+            }
+
+            if(mp.find(currstate)!=mp.end())
+            {
+                ans=max(ans,(i-mp[currstate]));
             }
             else
             {
-                mp[count]=i;
+                mp[currstate]=i;
             }
         }
 
-        
-
         return ans;
 
+        
     }
 };
