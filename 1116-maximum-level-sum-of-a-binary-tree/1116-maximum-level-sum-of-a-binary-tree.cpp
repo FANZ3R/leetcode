e35f5146531fc1c17fb745b72f6797904e2993fd
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-18 20:09:23 +0530

    Time: 134 ms (60.81%), Space: 104.5 MB (90.04%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,58 +11,47 @@
  */
 class Solution {
 public:
+
+    map<int,int>mp;
+
+    void DfsSum(TreeNode* root,int level)
+    {
+        if(root==NULL)
+        return ;
+
+        mp[level]+=root->val;
+
+        DfsSum(root->left,level+1);
+        DfsSum(root->right,level+1);
+
+    }
+
     int maxLevelSum(TreeNode* root) {
-        
-        int level =0;
+
+        //dfs se bhi karskta mai isko
+        //dfs mei level send kardunga 
+        //aur ek map rakhunga level,uska sum
+        //finally us level pe sum update karta rahunga
+
+        DfsSum(root,1);
+        //startign me level 1 rahega root ka
+
         int maxsum=INT_MIN;
+        int level=0;
 
-        //level order traversal karke hi karna hai aur har node ka sum lena hai theeke
-
-        queue<TreeNode*> q;
-
-
-        q.push(root);
-
-        
-        int l=1;
-
-        while(!q.empty())
+        for(auto t:mp)
         {
-              
-            int currlevel_sum=0;
+            int sum=t.second;
+            int l=t.first;
 
-            int n=q.size();
-
-            while(n--)
+            if(maxsum<sum)
             {
-                TreeNode* node=q.front();
-                currlevel_sum+=node->val;
-
-                q.pop();
-
-                if(node->left)
-                {
-                    q.push(node->left);
-                }
-
-                if(node->right)
-                {
-                    q.push(node->right);
-                }
-            }
-
-            //yaha pe end pe compare karunga kyuki sum acculate level ka while(n--) waale loop ke baad hora hoga
-
-            if(maxsum<currlevel_sum)
-            {
-                maxsum=currlevel_sum;
+                maxsum=sum;
                 level=l;
             }
-            l++;
-
         }
 
         return level;
+        
     }
-
 };
