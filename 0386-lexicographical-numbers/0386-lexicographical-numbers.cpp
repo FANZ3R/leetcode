22d468d1fdbd31ac794f6fb3a7c241687453fe60
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-22 14:53:51 +0530

    Time: 16 ms (54.75%), Space: 13.8 MB (68.28%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    vector<int> lexicalOrder(int n) {
 
-        //string ka comparator character ke basis pe karta hai
+    void solve(int x,int num ,vector<int> & result)
+    {
+        if(x > num)
+        return ;
 
-        auto lambda = [](int &a,int &b)
+        result.push_back(x);
+
+        //ab mai apend karunga recursively
+        //kyuki 1 pe 0 appendf karenge 10 fir 100 bhi hoskta agr hamara upper bound >100 hai toh
+
+        for(int append=0; append<=9;append++)
         {
-            string x = to_string(a);
-            string y = to_string(b);
-
-            if(x<y)
-            return true;
-
-            return false;
-        };
-
-        vector<int> ans(n,0);
-
-        for(int i=1;i<=n;i++)
-        {
-            ans[i-1]=i;
+            int newnum = x*10+append;
+            solve(newnum,num,result);
         }
 
-        sort(ans.begin(),ans.end(),lambda);
+    }
 
-        return ans;
+    vector<int> lexicalOrder(int n) {
+        // isko recursively solve karskte number append krke
+        //dekh 1 se 9 tak hi loop chaluanga baaki mera dfs recursive type ka code handle karlega
+        //muje append karna baaki fir number 10,100,11,110 aise karkte wale phle aayenge
+
+        vector<int> result;
+
+        int num=1;
+
+        for(int i=1;i<=9;i++)
+        {
+            
+            solve(i,n,result);
+        }
         
+
+        return result;
     }
 };
