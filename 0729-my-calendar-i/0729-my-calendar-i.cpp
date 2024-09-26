7d7c26f9c8b05f8ddf4c6c3a7fdd4d54fa03f1c2
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-27 04:36:03 +0530

    Time: 60 ms (97.5%), Space: 42.8 MB (58.59%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -4,6 +4,14 @@ public:
     
     set<pair<int,int>> st;
 
+    //ab isko mai aur optimise kar skta hu instead of checking by iterating through the whole list
+    //I can just check lower bound and compare it with the current pair
+
+    //agr mera lower bound ka start<curr node ka end mtlb overlap hai
+
+    //uar mai previous iterationb pe bhi jaunga
+    //agr mera previous iteration ka end> currnode ke start toh mtlb overlap hai 
+
     MyCalendar() {
 
         
@@ -14,30 +22,37 @@ public:
     
     bool book(int start, int end) {
 
-        for(auto t:st)
+        //pehle i will find the first eventhat starts after or st the same time of {start,end}
+
+        //mai lower bound se nikaal lunga element >=start or agr start se nhi milta toh end pe check karega ye code
+        auto it = st.lower_bound({start,end});
+
+        //it kispe point karega element >= start 
+        //aur agr nhi milta toh wo fir element >=end pe dhundega 
+
+        //aur agr wo bhi nhi milta toh end pe point karega yaani ki st.end() pe point karega
+
+        if(it != st.end() && it->first<end)
         {
-            int x=t.first;
-            int y=t.second;
-
-           // iska ye condition hota check karne 
-           //naye wale interval ka start already present ke end se compare karte
-           //aur already present ke start ko naye ke end se compare karte
-
-        //    if(start<y && x<end)
-        //    return false;
-
-        //    else if(start==x && end==y)
-        //    return false;
-
-
-        //iska ek aur tareek hota 
-
-        if(max(start,x) < min(end,y))
-        return false;
+            return false;
         }
 
-        st.insert({start,end});
+        //ab previous wale se check karunga ki previous se na overlap hojaaye
+        //toh agr previous ke lie condiiton ye hai ki previous hona bhi choaye
+        //agr st.begin() pe point karraha hoga toh previous nahi milega muje
 
+        if(it!=st.begin())
+        {
+            //phle prev wale pe point karte hai ab
+            auto prevIt = prev(it);
+            if(prevIt->second>start)
+            {
+                return false;
+            }
+        }
+
+        //aur agr nhi hua flase toh vlaid hai to insert kardunga apne set mei
+        st.insert({start,end});
         return true;
 
         
