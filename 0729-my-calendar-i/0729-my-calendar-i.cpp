75561883da9ff1ab15123efac7427041a2eb451a
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-26 13:31:03 +0530

    Time: 113 ms (29.41%), Space: 42.9 MB (36.63%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,45 @@
+class MyCalendar {
+public:
+
+    
+    set<pair<int,int>> st;
+
+    MyCalendar() {
+
+        
+       
+
+        
+    }
+    
+    bool book(int start, int end) {
+
+        for(auto t:st)
+        {
+            int x=t.first;
+            int y=t.second;
+
+           // iska ye condition hota check karne 
+           //naye wale interval ka start already present ke end se compare karte
+           //aur already present ke start ko naye ke end se compare karte
+
+           if(start<y && x<end)
+           return false;
+
+           else if(start==x && end==y)
+           return false;
+        }
+
+        st.insert({start,end});
+
+        return true;
+
+        
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
