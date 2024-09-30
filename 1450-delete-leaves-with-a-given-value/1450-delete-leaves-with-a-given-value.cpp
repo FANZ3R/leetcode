409d705015049cde23302178d84c2929b5d8b082
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-09-30 18:41:59 +0530

    Time: 11 ms (71.48%), Space: 22.9 MB (89.72%) - LeetHub

--- 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.cpp
@@ -30,15 +30,15 @@ public:
 
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
 
-        remove(root,target);
+       return  remove(root,target);
 
-        //mera function saare nodes ko to check karlega 
-        //but agr last mei root ko bhi delete karna pde 
-        //toh wo check muje finally khud se hi karna padega
-        if(root->left==NULL && root->right==NULL && root->val==target)
-        return NULL;
+        // //mera function saare nodes ko to check karlega 
+        // //but agr last mei root ko bhi delete karna pde 
+        // //toh wo check muje finally khud se hi karna padega
+        // if(root->left==NULL && root->right==NULL && root->val==target)
+        // return NULL;
 
-        return root;
+        // return root;
         
     }
 };
