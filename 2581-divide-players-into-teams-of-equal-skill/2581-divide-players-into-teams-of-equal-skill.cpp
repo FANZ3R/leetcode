8c638c5158cd57ba6b62f0c102604a1e1e3511f0
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-10-04 15:22:05 +0530

    Time: 66 ms (46.6%), Space: 57.2 MB (16.2%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        
 
-        // cout<<skill[0]<<endl;
         if(skill.size()==2)
         return skill[0]*skill[1];
 
-        // cout<<skill[0]<<endl;
         long long sum=0;
 
         unordered_map<int,int> mp;
@@ -21,57 +18,41 @@ public:
         int n=skill.size();
         int teams=n/2;
 
-        long long target =sum/teams;
-
-        //cout<<target<<endl;
-
         if(sum%teams!=0)
         return -1;
 
+        long long target=sum/teams;
 
         long long ans=0;
 
-        for(int j=0;j<n;j++)
+        for(int j=0;j<n;j++ )
         {
 
-          
-            long long remaining = target - skill[j];
-            // cout<<skill[j]<<endl;
-            // cout<<" "<<skill[j];
+            long long remaining =target-skill[j];
 
-              if(mp[skill[j]]==0 && mp[remaining]==0)
+            if(mp[skill[j]]==0 && mp[remaining]==0)
+            {
                 continue;
-
-
-            
-            // if(mp.find(remaining)!=mp.end()  && mp[j]==0 && mp[remaining]==0)
-            // continue; 
+            }
 
             mp[skill[j]]--;
 
-
-        
-            if(mp.find(remaining)!=mp.end() )
+            if(mp.find(remaining)!=mp.end())
             {
                 mp[remaining]--;
-                // cout<<skill[j]<<endl;
-                // cout<<remaining<<endl;
-
-                // cout<<skill[j]*remaining<<endl;
-                ans+=(skill[j] * remaining);
+                ans+=(skill[j]*remaining);
                 teams--;
-              
             }
-            
         }
 
-       if(teams==0)
-       return ans;
-
+        if(teams==0)
+        return ans;
 
         return -1;
-       
+
         
 
+
+        
     }
 };
