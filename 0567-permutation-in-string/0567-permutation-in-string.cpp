7e8c3cbf48e1c7dabad2c6bb6c74ba4254af510b
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-10-05 11:33:01 +0530

    Time: 1068 ms (6.19%), Space: 60.9 MB (9.23%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -7,20 +7,22 @@ public:
         //cout<<s1<<endl;
         string data=s2;
 
+        int n=s1.size();
+        int m=s2.size();
+
         
 
         int hop=s1.size();
 
-        for(int i=0;i<data.size();i++)
+        for(int i=0;i<=m-n;i++)
         {
-            if(i+hop<=data.size())
-            {
+            
                 string temp = data.substr(i,hop);
                 sort(temp.begin(),temp.end());
                 //cout<<temp<<endl;
                 if(temp == s1)
                 return true;
-            }
+            
         }
 
         return false;
