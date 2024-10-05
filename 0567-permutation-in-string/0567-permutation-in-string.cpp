da991a9b0119d989ce446dda3b35d57b620cb62e
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-10-05 11:38:50 +0530

    Time: 10 ms (41.85%), Space: 8.8 MB (50.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,27 +2,45 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
 
-        sort(s1.begin(),s1.end());
-
-        //cout<<s1<<endl;
-        string data=s2;
+        //sabse best ttareeka hai sliding window se karke 
+        //store karlunga frequency s1 ki vector mei 26 size ke 
+        //fir s1 ke size ka sliding window chalaunga s2 mei
+        //aur uski bhi frequency maintain karunga 
+        //agr frequecny ke vector same hue to true
 
         int n=s1.size();
         int m=s2.size();
 
-        
+        if(n>m)
+        return false;
 
-        int hop=s1.size();
+        vector<int> s1_freq(26,0);
+        vector<int> s2_freq(26,0);
 
-        for(int i=0;i<=m-n;i++)
+        for(char ch:s1)
         {
-            
-                string temp = data.substr(i,hop);
-                sort(temp.begin(),temp.end());
-                //cout<<temp<<endl;
-                if(temp == s1)
-                return true;
-            
+            s1_freq[ch-'a']++;
+        }
+
+        int i=0;
+        int j=0;
+
+        while(j<m)
+        {
+            s2_freq[s2[j]-'a']++;
+
+            //ab muje shrink kab karni hai window 
+            //jab mera sliding window ka size s1 ki string se bada hojaeega
+            if(j-i+1>n)
+            {
+                s2_freq[s2[i]-'a']--;
+                i++;
+            }
+
+            if(s1_freq == s2_freq)
+            return true;
+
+            j++;
         }
 
         return false;
