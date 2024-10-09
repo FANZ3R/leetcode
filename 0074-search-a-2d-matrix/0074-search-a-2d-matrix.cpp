ef1b19226ebf95bf9998a6bfad913cab9baeb957
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-10-10 02:58:06 +0530

    Time: 2 ms (68.29%), Space: 12.2 MB (41.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        // ab efficiently solve karne ke lie wo bhi logarithmic mei
+        // dekh tere dimaag mei hamesa aana chaiye kyuki sorted hai toh binary search 
+        //abs is question mai bas muje mapping ka trick pata karna hai 1 d index ko 2 d mai
+        //toh jo trick hota hai wo 1d (index/number of cols) se gives me its respective row
+        //and 1d index%number of rows gfives me its respective column
+        //toh bas yehi use karna hai
 
         int m=matrix.size();
         int n=matrix[0].size();
 
-        for(int i=0;i<m;i++)
+        int start = 0;
+        int end = m*n-1;
+
+
+        while(start<=end)
         {
-            int small= matrix[i][0];
-            int larger = matrix[i][n-1];
+            int mid= start +(end-start)/2;
 
-            if(larger>=target && small<=target)
+            if(matrix[mid/n][mid%n] == target)
             {
-                for(int j=0;j<n;j++)
-                {
-                    if(matrix[i][j]==target)
-                    return true;
-                }
-
-                return false;
+                return true;
             }
-        }
+            else if(matrix[mid/n][mid%n] > target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        } 
 
         return false;
-        
     }
 };
