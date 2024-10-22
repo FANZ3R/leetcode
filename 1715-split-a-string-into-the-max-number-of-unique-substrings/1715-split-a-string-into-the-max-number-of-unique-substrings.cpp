92c413b61fcc4ef62df1939b3eaf8900aa6396e9
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-10-22 09:40:08 +0530

    Time: 43 ms (93.66%), Space: 18.4 MB (91.64%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
 
-    void solve(string s,unordered_set<string> &st, int i,int currcount,int &maxcount)
+    void solve(string s,int i, unordered_set<string>&st,int currcount,int &maxcount)
     {
-        //ab isme pruning bhi daaal skta
+              //ab isme pruning bhi daaal skta
         //kyuki agr isi bhi index pe hu toh uske baad max unique string kitne mil skte jitne character hone
         //wo max tabhi milenge jab teeno unique characters honge
 
@@ -15,44 +15,44 @@ public:
         return ; 
 
 
-
-        if(i == s.size())
+        if(i>=s.size())
         {
-            maxcount = max(currcount , maxcount);
-            return ;
+            maxcount =max(maxcount,currcount);
+            return;
         }
 
         for(int j=i;j<s.size();j++)
         {
             string sub = s.substr(i,j-i+1);
-
-            if(st.find(sub) == st.end())
+            if(st.find(sub)==st.end())
             {
-                //do 
-                st.insert(sub);
-
-                //explore
-                solve(s,st,j+1,currcount+1,maxcount);
-
-                //undo
-                st.erase(sub);
+                st.insert(sub); //DO
+                solve(s,j+1,st,currcount+1,maxcount); //EXPLORE
+                st.erase(sub); //UNDO
             }
         }
     }
 
-
     int maxUniqueSplit(string s) {
+        //dekh isme choice hai har character pe ki yaha pe split karna hai ki nahi
+        //aur pehele dkha ki nahi uske lie set of strings rakh lunga mai
+        //but kya pata aage split karne se ajada shi milte toh
+        //set of strings uske lie alga aayega
+        //iske lie backtracking lagaunga
+        //do explore and then undo
+        //kyuki string ki length ka constraint bhi kam hai 16 toh lag jaayega ispe
+
 
         unordered_set<string> st;
 
-        int currcount = 0;
-        int maxcount = 0;
+        int currcount=0;
+        int maxcount=0;
+
         int i=0;
 
-        solve(s,st,i,currcount,maxcount);
+        solve(s,i,st,currcount,maxcount);
 
         return maxcount;
-
         
     }
 };
