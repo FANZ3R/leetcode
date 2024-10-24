466c28b16d52e6f2ec52ecddb62b45bb04163931
Abhinav <72151247+FANZ3R@users.noreply.github.com>
2024-10-24 09:38:42 +0530

    Time: 0 ms (100%), Space: 14.5 MB (84.24%) - LeetHub

--- 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.cpp
+++ 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.cpp
@@ -13,31 +13,30 @@ class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
 
-        //dekh muje values check karni padegi poore equivalent ke lie
-        if(root1 ==NULL && root2==NULL)
+        //agr dono null hue
+        if(root1 == NULL && root2 == NULL)
         return true;
 
-        if(root1 == NULL || root2==NULL)
+        //sirf ek null hua
+
+        if(root1 == NULL || root2 == NULL)
         return false;
+
+        //dekh values pe bhi check karna padega
+        //agr root ki values hi match ni karri toh flip karne ka faayeda ni wo waise equal nhi 
 
         if(root1->val != root2->val)
         return false;
 
+        //ab dekh flip kr bhi ksta aur nhi bhi
+        //toh ek baar mai flipped wale pe check karunga aur ek baar notflipped
+        //ab dono hi shi hoskte toh agr ek bhi true mila dono me se toh sahi hai true hai
 
-        bool check_not_flipped ;
-        //ye check tbke lie jab root1->left aur root2->left same honge aur similarly root2 bbhi
-        //mtlb flip nhi kiya hua
+        bool not_flipped = flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right);
 
-        check_not_flipped = flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right);
+        bool flipped     = flipEquiv(root1->left,root2->right) && flipEquiv(root1->right,root2->left);
 
-        bool check_flipped;
-
-        //jab dono flipped honge
-
-        check_flipped = flipEquiv(root1->left, root2->right) && flipEquiv(root1->right,root2->left);
-
-        return check_not_flipped || check_flipped;
-       
+        return flipped || not_flipped;
         
     }
 };
